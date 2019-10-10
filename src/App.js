cfd4369e462d19f26d3369a4bdcf96886c75716c
github username <github email address>
2019-10-10 13:45:49 -0400

    Added Slider package and created function to control color level

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Palette from './components/Palette'
 import seedColors from './seeds/seedColors'
+import {generatePalette} from "./color helper/colorHelper"
 
 export default function App() {
   return (
     <div>
-      <Palette {...seedColors[4]}/>
+      <Palette pallete={generatePalette(seedColors[4])}/>
     </div>
   )
 }
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ColorBox from './ColorBox'
+
+import Slider from "rc-slider"
+import 'rc-slider/assets/index.css'
 import "../css/palette.css"
 
+
 export default function Pallete(props) {
-    const colorBoxes = props.colors.map( (color) =>(
+    const colors = props.pallete.colors
+    const [colorLevel, setColorLevel] = useState(500)
+
+   
+    const colorBoxes = colors[colorLevel].map((color)=>(
         <ColorBox color={color} />
+        )
     )
-)
+
+    const changeLevel = (level) => ( setColorLevel(level) )
+    
+    
     return (
         <div className="Pallete">
+            <Slider 
+                defaultValue={colorLevel}
+                min={100} max={900} 
+                step={100}
+                onAfterChange={changeLevel}
+            />
             <div className="Pallete-colors">
                 {colorBoxes}
+                {console.log(props.pallete)}
             </div>
         </div>
     )
